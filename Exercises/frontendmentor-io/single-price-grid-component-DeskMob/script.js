9f04144a1eb1f0ff1f55ddd3a163142acf669c97
GHNetCode <125825587+GHNetCode@users.noreply.github.com>
2023-03-06 14:38:27 +0000

    Update script.js

--- Exercises/frontendmentor-io/single-price-grid-component-DeskMob/script.js
+++ Exercises/frontendmentor-io/single-price-grid-component-DeskMob/script.js
@@ -3,11 +3,17 @@
 // document.querySelector('#resizeInnerW').textContent=window.innerWidth;
 // }
 
-const btnC = document.getElementById("idhState");
-btnC.addEventListener('touchstart', () => btnC.style.background = 'blue')
-btnC.addEventListener('touchmove', () => btnC.style.background = 'red')
-btnC.addEventListener('touchend', () => btnC.style.background = 'green')
+// const btnC = document.getElementById("idhState");
+// btnC.addEventListener('touchstart', () => btnC.style.background = 'blue')
+// btnC.addEventListener('touchmove', () => btnC.style.background = 'red')
+// btnC.addEventListener('touchend', () => btnC.style.background = 'green')
 
+const btnC = document.getElementById("idhState");
+btnC.addEventListener('touchstart', () => btnC.style.background = 'rgb(160, 194, 23)')
+//btnC.addEventListener('touchmove', () => btnC.style.background = 'red')
+btnC.addEventListener('touchend', () => btnC.style.background = 'rgb(160, 194, 23)')
+
+//hsl(71, 73%, 54%)
 
 // function myFunkyB(){
 // alert("You have clicked the Button!");
