731759930efe754e7e01a50d0d9d1d11f0b27fc5
GHNetCode <125825587+GHNetCode@users.noreply.github.com>
2023-03-06 13:27:46 +0000

    Update script.js

--- Exercises/frontendmentor-io/single-price-grid-component-DeskMob/script.js
+++ Exercises/frontendmentor-io/single-price-grid-component-DeskMob/script.js
@@ -1,7 +1,16 @@
-// function myFunkyB(){
-// alert("You have clicked the Button!");
+
+// window.onresize =() =>{
+// document.querySelector('#resizeInnerW').textContent=window.innerWidth;
 // }
 
-window.onresize =() =>{
-document.querySelector('#resizeInnerW').textContent=window.innerWidth;
+function myFunkyB(){
+alert("You have clicked the Button!");
 }
+
+
+function changeBackground(color) {
+   // document.body.style.background = color;
+    document.getElementById("#idhState").style.background=color;
+ }
+  document.getElementById("#idhState").ontouchstart = changeBackground('red');
+  document.getElementById("#idhState").ontouchend = changeBackground('blue');
