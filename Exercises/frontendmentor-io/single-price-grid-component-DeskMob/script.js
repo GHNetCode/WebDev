b33dedb5b0e4aa6f0f6bc92814be2f863e691130
GHNetCode <125825587+GHNetCode@users.noreply.github.com>
2023-03-06 14:25:52 +0000

    Update script.js

--- Exercises/frontendmentor-io/single-price-grid-component-DeskMob/script.js
+++ Exercises/frontendmentor-io/single-price-grid-component-DeskMob/script.js
@@ -3,14 +3,20 @@
 // document.querySelector('#resizeInnerW').textContent=window.innerWidth;
 // }
 
+const btnC = document.getElementById("#idhState");
+btnC.addEventListener('touchstart', () => btnC.style.background = 'red')
+btnC.addEventListener('touchmove', () => btnC.style.background = 'blue')
+btnC.addEventListener('touchend', () => btnC.style.background = 'red')
+
+
 // function myFunkyB(){
 // alert("You have clicked the Button!");
 // }
 
 
-function changeBackground(color) {
-   // document.body.style.background = color;
-    document.getElementById("idhState").style.background=color;
- }
-  document.getElementById("idhState").ontouchstart = changeBackground('red');
+// function changeBackground(color) {
+//    // document.body.style.background = color;
+//     document.getElementById("idhState").style.background=color;
+//  }
+//   document.getElementById("idhState").ontouchstart = changeBackground('red');
   //document.getElementById("idhState").ontouchend = changeBackground('blue');
