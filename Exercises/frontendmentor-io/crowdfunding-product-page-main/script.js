35f379ecf5efd178fd96444104bfa4dae10e661c
GHNetCode <125825587+GHNetCode@users.noreply.github.com>
2023-03-31 11:25:47 +0100

    Add files via upload

--- Exercises/frontendmentor-io/crowdfunding-product-page-main/script.js
+++ Exercises/frontendmentor-io/crowdfunding-product-page-main/script.js
@@ -5,30 +5,19 @@ document.querySelector('#resizeInnerW').textContent=window.innerWidth;
 // Get the modal
 let modal = document.getElementById("myModal"); //not in css..
 
-// When the user clicks anywhere except on the links 
-  // inside of the menu toggle, close it..
-  let checkBinput = document.getElementById('checkB');
+// 'X'\check Button for menu toggle
+let checkBinput = document.getElementById('checkB');
+
+let boxConBtnCls = document.getElementsByClassName('boxConBtnCls');
 
 
 //check all events for "click" and "touchstart" and handle accordingly..
   if ('ontouchstart' in window) {
-         document.addEventListener("touchstart", (event) => {  
-          // Retrieve element id from touches element..
-          //let elementId = event.target.id;
-            // If element has id
-            //  if (elementId !== '') {
-            //  console.log(elementId);
-            //  }
-            // If element has no id
-            //  else { 
-            //  console.log("An element without an id was clicked.");
-            //  }
-
+         document.addEventListener("touchstart", (event) => { //For touch screens only.. 
                 let elementId = event.target.id;
                 let elementCls = event.target.classList; //event.target.classList.contains("my-class")
                   console.log("all elementId's :"+ elementId);
                   console.log("all elementCls's :"+ elementCls);
-
 
           //For the Menu (hamburger). -:         
                 //When the user touches anywhere except on the links  
@@ -37,28 +26,33 @@ let modal = document.getElementById("myModal"); //not in css..
                 //  console.log('for checkB : '+ elementId);
                 checkBinput.checked = false;
                 }
-
-
           //For the modal/dialogue. -:
                 //Lets close the modal(dialogue) if user touches on the 'x' button.
                 if (elementId == 'myModalCloseBtn'){ //for spanCloseEid
                 modal.style.display = "none";
                 }
-                //Lets close the modal(dialogue) if user touches outside of the modal window..
-                //Note: Disabling for touchscreens..
-              //  if (elementId == 'myModal') {
-              //   modal.style.display = "none";
-              //  }
-
-                //Lets open the modal(dialogue)..
+                //Lets give color to the buttons and open the modal(dialogue)..
                 if (elementCls == 'boxConBtnCls') {
-                modal.style.display = "flex";
-                }                
+                      boxConBtnCls.style.background = 'rgb(76, 201, 193)';
+                      boxConBtnCls.style.color = 'white';
+                      modal.style.display = "flex";
+                }
+
             });
+      } else if ('ontouchend' in window){
+        document.addEventListener("touchend", (event) => {
+        let elementCls = event.target.classList;
+
+            if (elementCls == 'boxConBtnCls') {
+                boxConBtnCls.style.background = 'rgb(60, 180, 171)';
+                boxConBtnCls.style.color = 'white';
+            //  modal.style.display = "flex";
+                }
+
+             });
 
       } else {
-         document.addEventListener("click", (event) => { 
-
+         document.addEventListener("click", (event) => { // For mouse clicks only..
           let elementId = event.target.id;
           let elementCls = event.target.classList; //event.target.classList.contains("my-class")
           console.log("all elementId's :"+ elementId);
@@ -72,8 +66,6 @@ let modal = document.getElementById("myModal"); //not in css..
           console.log('for checkB : '+ elementId);
           checkBinput.checked = false;
         }
-
-
     //For the modal/dialogue. -:
         //Lets close the modal(dialogue) if user clicks on the 'x' button.
         if (elementId == 'myModalCloseBtn'){ //for spanCloseEid
@@ -87,9 +79,7 @@ let modal = document.getElementById("myModal"); //not in css..
          //Lets open the modal(dialogue)..
          if (elementCls == 'boxConBtnCls') {
           modal.style.display = "flex";
-
          }
-         
       });
    }
 
