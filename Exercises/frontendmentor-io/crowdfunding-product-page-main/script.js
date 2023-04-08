5fe512979ccdd48eb1baba54c10d54b9aaff04a9
GHNetCode <125825587+GHNetCode@users.noreply.github.com>
2023-04-08 21:36:18 +0100

    Add files via upload

--- Exercises/frontendmentor-io/crowdfunding-product-page-main/script.js
+++ Exercises/frontendmentor-io/crowdfunding-product-page-main/script.js
@@ -12,6 +12,8 @@ let boxConBtnCls = document.getElementsByClassName('boxConBtnCls');
 
 //modulas radio buttons to handle the boxes and pledge sums..
 let inpBtnStyler = document.getElementsByClassName('inpBtnStyler');
+let mdlCont1 = document.getElementById('mdl-Cont1');
+let mdlCont1Pld1 = document.getElementById('mdl-Cont1Pld1');
 let mdlCont2 = document.getElementById('mdl-Cont2');
 let mdlCont2Pld1 = document.getElementById('mdl-Cont2Pld1');
 let mdlCont3 = document.getElementById('mdl-Cont3');
@@ -62,68 +64,78 @@ let touchduration = 150; //length of time ..
                     boxConBtnCls.style.color = 'white';
                 }
 
-                         //remember to put this in for mobile after testing!!
-                         if (elementCls == 'inpBtnStyler') {
-                          //Give a bit of time between changing the color 'active color' of the button
-                          //and firing the modal\dialogue..
-                          // timer = setTimeout(timedTouchFunc, touchduration);
-                         
-                        
-                          for (let i = 0; i < inpBtnStyler.length; i++) {
-                              //  let rBtn = inpBtnStyler[i];
-                              //console.log('button inside forloop..'+inpBtnStyler[i].id)
+         //remember to put this in for mobile after testing!!
+         if (elementCls == 'inpBtnStyler') {
+          //Give a bit of time between changing the color 'active color' of the button
+          //and firing the modal\dialogue..
+          // timer = setTimeout(timedTouchFunc, touchduration);
+         
+        
+          for (let i = 0; i < inpBtnStyler.length; i++) {
+              //  let rBtn = inpBtnStyler[i];
+              //console.log('button outside forloop..'+inpBtnStyler[i].id)
+              if (inpBtnStyler[i].checked){
+               //console.log('button selected'+inpBtnStyler[i].id)
+
+              //Handle the events for pledges when radio button is pushed..
+
+               // Grab button id for This box\div.. mdl-Cont2 -- Bamboo Stand
+               if (inpBtnStyler[i].id ==='mdl-Cont1Rbtn'){ 
+                // console.log('button selected-ifstatement'+inpBtnStyler[i].id)
+                
+                mdlCont1.style.height='350px'; //make the window longer to accommodate for the buttons etc..  
+                mdlCont1.style.border= 'rgb(59, 180, 171) solid 2px';
+                mdlCont1Pld1.style.display='flex';
+              } else {
+                mdlCont1.style.height='244px'; //make the window longer to accommodate for the buttons etc..  
+                mdlCont1.style.border= 'rgb(218, 218, 218) solid 2px'; //'unset' no worky.. :(
+                mdlCont1Pld1.style.display='none'; // Set back to none / Flex!
+               }
+
+                  // Grab button id for This box\div.. mdl-Cont2 -- Bamboo Stand
+                  if (inpBtnStyler[i].id ==='mdl-Cont2Rbtn'){ 
+                    // console.log('button selected-ifstatement'+inpBtnStyler[i].id)
+                    
+                    mdlCont2.style.height='422px'; //make the window longer to accommodate for the buttons etc..  
+                    mdlCont2.style.border= 'rgb(59, 180, 171) solid 2px';
+                    mdlCont2Pld1.style.display='flex';
+                  } else {
+                    mdlCont2.style.height='unset'; //make the window longer to accommodate for the buttons etc..  
+                    mdlCont2.style.border= 'rgb(218, 218, 218) solid 2px'; //'unset' no worky.. :(
+                    mdlCont2Pld1.style.display='none'; // Set back to none / Flex!
+                   }
+
+                  // Grab button id for This box\div.. mdl-Cont3 -- Black Edition Stand
+                  if (inpBtnStyler[i].id ==='mdl-Cont3Rbtn'){ 
+                    // console.log('button selected-ifstatement'+inpBtnStyler[i].id)
+                    mdlCont3.style.height='422px'; //make the window longer to accommodate for the buttons etc..  
+                    mdlCont3.style.border= 'rgb(59, 180, 171) solid 2px'
+                    mdlCont3Pld1.style.display='flex';
+
+                  } else {
+                    mdlCont3.style.height='unset';
+                    mdlCont3.style.border= 'rgb(218, 218, 218) solid 2px';
+                    mdlCont3Pld1.style.display='none'; // Set back to none / Flex!
+                   }
+
+                   // Grab button id for This box\div.. mdl-Cont4 -- Mahogany Special Edition
+                  if (inpBtnStyler[i].id ==='mdl-Cont4Rbtn'){
+                    // console.log('button selected-ifstatement'+inpBtnStyler[i].id)
+                    
+                    mdlCont4.style.height='422px'; //make the window longer to accommodate for the buttons etc..  
+                    mdlCont4.style.border= 'rgb(59, 180, 171) solid 2px'
+                    mdlCont4Pld1.style.display='flex';
+
+                  } else {
+                    mdlCont4.style.height='unset';
+                    mdlCont4.style.border= 'rgb(218, 218, 218) solid 2px';
+                    mdlCont4Pld1.style.display='none'; // Set back to none / Flex!
+                    }
 
 
-    //per below 'checked' is Valid 'checked' in for loop and 'ontouch' correspond ok...!!!!!!!!!!
-                              if (inpBtnStyler[i].checked){
-                               //console.log('button selected'+inpBtnStyler[i].id)
-                
-                              //Handle the events for pledges when radio button is pushed..
-                                  //DIVS ARE NOT being picked up correctly for each button..
-                                  // Grab button id for This box\div.. mdl-Cont2 -- Bamboo Stand
-                                  if (inpBtnStyler[i].id ==='mdl-Cont2Rbtn'){ 
-                                    // console.log('button selected-ifstatement'+inpBtnStyler[i].id)
-                                    
-                                    mdlCont2.style.height='422px'; //make the window longer to accommodate for the buttons etc..  
-                                    mdlCont2.style.border= 'rgb(59, 180, 171) solid 2px';
-                                    mdlCont2Pld1.style.display='flex';
-                                  } else {
-                                    mdlCont2.style.height='unset'; //make the window longer to accommodate for the buttons etc..  
-                                    mdlCont2.style.border= 'rgb(218, 218, 218) solid 2px'; //'unset' no worky.. :(
-                                    mdlCont2Pld1.style.display='none'; // Set back to none / Flex!
-                                   }
-                
-                                  // Grab button id for This box\div.. mdl-Cont3 -- Black Edition Stand
-                                  if (inpBtnStyler[i].id ==='mdl-Cont3Rbtn'){ 
-                                   //  console.log('button selected-ifstatement'+inpBtnStyler[i].id)
-                                    mdlCont3.style.height='422px'; //make the window longer to accommodate for the buttons etc..  
-                                    mdlCont3.style.border= 'rgb(59, 180, 171) solid 2px'
-                                    mdlCont3Pld1.style.display='flex';
-                
-                                  } else {
-                                    mdlCont3.style.height='unset';
-                                    mdlCont3.style.border= 'rgb(218, 218, 218) solid 2px';
-                                    mdlCont3Pld1.style.display='none'; // Set back to none / Flex!
-                                   }
-                
-                                   // Grab button id for This box\div.. mdl-Cont4 -- Mahogany Special Edition
-                                  if (inpBtnStyler[i].id ==='mdl-Cont4Rbtn'){
-                                   //  console.log('button selected-ifstatement'+inpBtnStyler[i].id)
-                                    
-                                    mdlCont4.style.height='422px'; //make the window longer to accommodate for the buttons etc..  
-                                    mdlCont4.style.border= 'rgb(59, 180, 171) solid 2px'
-                                    mdlCont4Pld1.style.display='flex';
-                
-                                  } else {
-                                    mdlCont4.style.height='unset';
-                                    mdlCont4.style.border= 'rgb(218, 218, 218) solid 2px';
-                                    mdlCont4Pld1.style.display='none'; // Set back to none / Flex!
-                                    }
-                
-                
-                                }
-                             }
-                         }
+                }
+             }
+         }
                 
 
             });
@@ -184,6 +196,19 @@ let touchduration = 150; //length of time ..
                //console.log('button selected'+inpBtnStyler[i].id)
 
               //Handle the events for pledges when radio button is pushed..
+
+               // Grab button id for This box\div.. mdl-Cont2 -- Bamboo Stand
+               if (inpBtnStyler[i].id ==='mdl-Cont1Rbtn'){ 
+                // console.log('button selected-ifstatement'+inpBtnStyler[i].id)
+                
+                mdlCont1.style.height='320px'; //make the window longer to accommodate for the buttons etc..  
+                mdlCont1.style.border= 'rgb(59, 180, 171) solid 2px';
+                mdlCont1Pld1.style.display='flex';
+              } else {
+                mdlCont1.style.height='244px'; //make the window longer to accommodate for the buttons etc..  
+                mdlCont1.style.border= 'rgb(218, 218, 218) solid 2px'; //'unset' no worky.. :(
+                mdlCont1Pld1.style.display='none'; // Set back to none / Flex!
+               }
 
                   // Grab button id for This box\div.. mdl-Cont2 -- Bamboo Stand
                   if (inpBtnStyler[i].id ==='mdl-Cont2Rbtn'){ 
