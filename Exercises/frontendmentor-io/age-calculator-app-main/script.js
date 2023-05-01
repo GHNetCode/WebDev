2a861ad9b7a0f7f54898b0d6df9e1b49395e80ad
GHNetCode <125825587+GHNetCode@users.noreply.github.com>
2023-05-01 14:17:50 +0100

    Add files via upload

--- Exercises/frontendmentor-io/age-calculator-app-main/script.js
+++ Exercises/frontendmentor-io/age-calculator-app-main/script.js
@@ -25,15 +25,16 @@ if ('ontouchstart' in window){
         console.log('touchstart all  elementCls :'+ elementCls)
         console.log('touchstart all  elementId :'+ elementId);
 
-        if(elementId ==='DayBoxLbLP'||'DayBoxP'||'rangeDayId'){
-            console.log('touchstart DayBoxLbLP-DayBoxP-rangeDayId If statement..' );
-            rangeDayId.style.display='flex';
+        if(elementId ==='DayBoxLbLP'){ //||'DayBoxP'||'rangeDayId'){
+            console.log('touchstart DayBoxLbLP If statement..' );
+           // rangeDayId.style.display='flex';
             DayBoxLbLP.style.display='none';//MM Label
         }else{
-            rangeDayId.style.display='none';
+          //  rangeDayId.style.display='none';
 
-            if (DayBoxP.value===''){
-            DayBoxLbLP.style.display='flex';}
+         //   if (DayBoxP.value===''){
+            DayBoxLbLP.style.display='flex';
+       // }
         }
 
 
