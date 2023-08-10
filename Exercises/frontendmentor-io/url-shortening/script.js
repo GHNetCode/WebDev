c91867371092ef4e24e3338bdfceb7eba0a98f70
GHNetCode <125825587+GHNetCode@users.noreply.github.com>
2023-08-10 19:34:24 +0100

    Add files via upload

--- Exercises/frontendmentor-io/url-shortening/script.js
+++ Exercises/frontendmentor-io/url-shortening/script.js
@@ -5,7 +5,7 @@ document.getElementById('resizeInnerW').textContent=window.innerWidth;
 
 
 // To allow more Links (or Rows) to be added
-// just need to adjust the maxRows parameter..
+// just need to adjust the 'maxRows' parameter..
 
 let error = '';
 let parentElemct1D1 = document.getElementById("ct1D1"); //Div for ct1D1inp, ct1D1Btn
@@ -27,12 +27,13 @@ let oneClickFlag=false; // For function plsAddLnkMsg() "Please add a link"..
 let iKey = 0; // Key Id for Local storage to keep track of buttons. This value always increments even when the oldest link is replaced..
 
 
-//As we need to wait 1 second for this the request lets had some visual effects on the button
+// As we need to wait 1 second for this the request, lets had some visual effects 
+// on the button
 // 'Shorten IT!' button..
-//Setup Animations for the spinning arrow.:
+// Setup Animations for the spinning arrow.:
 const effect = new KeyframeEffect(//for Button
 ct1D1Btn, // Element to animate.. background-color(lightblue)
-[{transform: 'rotate(0deg) scalex(0.25)'},{transform: 'rotate(5000deg) scalex(0.1)'}], //,{transform: 'scalex(1)'},{transform: 'scalex(2)'}],// Keyframes
+[{transform: 'rotate(0deg) scalex(0.17)'},{transform: 'rotate(3600deg) scalex(0.0)'}], //,{transform: 'scalex(1)'},{transform: 'scalex(2)'}],// Keyframes
 {duration: 1000} // Keyframe settings   1sec..  
 );
 const rotatect1D1Btn = new Animation(effect, document.timeline);
@@ -49,21 +50,14 @@ console.log('ct1D1Btn Button has been pressed..')
 //console.log('Url text field..:'+ct1D1inp.value)
 
 async function BtnProc(){//process functions for Button "Shorten It!"
-
-
-if (ct1D1inp.value){//ct1D1inp - check if we have data and process it..
+ if (ct1D1inp.value){//ct1D1inp - check if we have data and process it..
     console.log('we have data:'+ct1DlongUrl);
     ct1DlongUrl=ct1D1inp.value;
 
-     rotatect1D1Btn.play();
-
-
-         oneClickFlag=false;//reset oneClickFlag..:
-         error = ''; //reset error flag....:
-
-
-          urlValidator(ct1DlongUrl) //Validate url and then call func UrlLinkDiv to shorten it..
-            
+      rotatect1D1Btn.play();
+      oneClickFlag=false;//reset oneClickFlag..:
+      error = ''; //reset error flag....:
+      urlValidator(ct1DlongUrl); //Validate url and then call func UrlLinkDiv to shorten it..
 
         if (error===''){// Url has been validated.. 
 
@@ -100,8 +94,9 @@ BtnProc();
 })
 
 
-
-function plsAddLnkMsg(){//function to create&display err msg and style input element.. ct1D1 is the parent, insert "ct1D1AdLnkMsg" between id`s ct1D1inp & ct1D1Btn
+//function to create&display err msg and style input element.. ct1D1 is 
+//the parent, insert "ct1D1AdLnkMsg" between id`s ct1D1inp & ct1D1Btn
+function plsAddLnkMsg(){
     let childElem = document.getElementById("ct1D1Btn");
     let textnode = document.createTextNode("Please add a link");
     // Create new & style p element to put our textnode in..
@@ -125,7 +120,6 @@ function plsAddLnkMsg(){//function to create&display err msg and style input ele
     ct1D1inp.style.border="inset 3px rgba(255, 0, 0, 0.500)"; //set back to none..
     parentElemct1D1.style.height="182px";
     ct1D1Btn.style.top="110px"
-    //Remember to adjust height of button ct1D1Btn and ct1D1 parent container..
 
 }
 
@@ -144,7 +138,6 @@ function resetErrStyles(){
     ct1D1Btn.style.top="86px"
 }
 ct1D1inp.oninput=function(){
-   // console.log("We got input!! --:"+ct1D1inp.value[0])
  if (ct1D1inp.value===ct1D1inp.value[0]){//Only on the first character lets reset the params..
     resetErrStyles();
     }
@@ -152,7 +145,6 @@ ct1D1inp.oninput=function(){
 
 //Ensure error is also cleared when pasting occurs..
 ct1D1inp.onpaste=function(){
-       // console.log('onpaste -:'+ct1D1inp.value);
         resetErrStyles();
 };
 
@@ -164,10 +156,6 @@ ct1D1inp.onpaste=function(){
         try {
           const response = await fetch('https://api.shrtco.de/v2/shorten?url='+ct1DlongUrl,{ signal: AbortSignal.timeout(2000)});// timeout in 2 seconds..
           const data = await response.json();
-          //console.log('data:', JSON.stringify(data));
-          //console.log('getShortUrl data.result.short_link3:'+data.result.short_link3);
-          //ct1DshortUrl=data.result.short_link3;
-          //console.log('getShortUrl data.result.short_link3:'+ct1DshortUrl);
         return (data.result.short_link3)
         } catch (err) {
             error = err;
@@ -186,9 +174,6 @@ async function urlValidator(ct1DlongUrl){
     try {
           new URL(ct1DlongUrl);//Validate the Url against the URL constructor
           console.log('We have a valid Url, Great!:'+ct1DlongUrl);
-        //  ct1DshortUrl = await (getShortUrl(ct1DlongUrl)) //return short url 'ct1DshortUrl'...
-        //  .then (console.log('urlValidator  data.result.short_link3:'+ct1DshortUrl))
-          //.then (UrlLinkDiv(ct1DlongUrl,ct1DshortUrl));
 
           return true;
       } catch (err) {
@@ -232,12 +217,9 @@ function UrlLinkDiv(ct1D1CpyLnkBtnId,ct1D1ShrtLnkPId,ct1DlongUrl,ct1DshortUrl){/
     // Get parent's child where we want to insert the new div next too..
     let childElem = document.getElementById("ct1D2");
     
-console.log("UrlLinkDiv -- OnLoadLclStrgFlagiKey..:"+OnLoadLclStrgFlagiKey)
-console.log("UrlLinkDiv -- iKey..:"+iKey)
 
 
-
-  //Notifications for Deleting the oldest link (first one in the list) only apply After 
+  //prompt Notifications for Deleting the oldest link (first one in the list) only apply After 
   //pressing the 'Shorten it!' button in this function UrlLinkDiv()..
       // Before cloning check max number of 'links (children)' already added on the page ..
       // if above threshold advise to copy\save old link...
@@ -245,45 +227,23 @@ console.log("UrlLinkDiv -- iKey..:"+iKey)
       console.log('UrlLinkDiv -: ct1D has '+numChd+' children...........');//num of children cloned..
   
       if (numChd > (7+maxRows) ){//(numChd >11)   7+maxRows=11..
-      console.log("UrlLinkDiv numChd iKey ----------- :"+iKey  );
-      console.log("UrlLinkDiv numChd maxRows ----------- :"+maxRows  );
-      
+
            //NOTE iKey[] has Already been incremented.
           if (iKey===maxRows){
               if (document.getElementById("ct1D1ShrtLnkP")){// The first child id there is no num appended..
 
-
-                console.log("UrlLinkDiv numChd (iKey - maxRows)  ----------- :"+(iKey - maxRows) );
-                console.log('UrlLinkDiv numChd '+document.getElementById("ct1D1ShrtLnkP"+(iKey - maxRows)));
-                  // console.log("UrlLinkDiv Here we are Deleting.. iKey:"+iKey  );//if ikey is 0 use ct1D1ShrtLnkP..
-                   console.log("UrlLinkDiv numChd ct1D1LsN1Cld - Here we are Deleting.. ct1D1LsN1Cld:"+(iKey - maxRows)  )
-  
                    prompt(maxnumLnksMsg,"Old link -: https://"+document.getElementById("ct1D1ShrtLnkP").innerText)
   
-                    console.log("UrlLinkDiv numChd short link to be deleted ct1D1ShrtLnkP :"+document.getElementById("ct1D1ShrtLnkP").innerText);
-                    console.log('UrlLinkDiv numChd ')
-                    
                     let ct1D1LsN1CldiKey = document.getElementById("ct1D1LsN1Cld"+(iKey - maxRows));
-                    console.log("UrlLinkDiv numChd ct1D1LsN1Cld"+(iKey - maxRows));
                     ct1D1LsN1CldiKey.parentNode.removeChild(ct1D1LsN1CldiKey);
-                    console.log("UrlLinkDiv numChd0 ct1D1CpyLnkBtnArr -:"+ct1D1CpyLnkBtnArr);
                     ct1D1CpyLnkBtnArr.splice(0,2);//Lets remove the first 2 elements from our array..
-                    console.log("UrlLinkDiv numChd1 ct1D1CpyLnkBtnArr -:"+ct1D1CpyLnkBtnArr);
-  
                   }
               }else if(iKey > maxRows ){
 
-                  // Here we have numbers that are appended to ct1D1ShrtLnkP id`s where 
-                  // iKey is above maxRows..
+                  // Here we have numbers that are appended to ct1D1ShrtLnkP id`s
                   // Because iKey has already been incremented for id 'ct1D1LsN1Cld'
                   // let's decrease it by 1 for id 'ct1D1ShrtLnkP'
                   
-                  //ct1D1ShrtLnkP
-                  console.log("UrlLinkDiv numChd iKey... :"+iKey)
-                  console.log("UrlLinkDiv numChd maxRows... :"+maxRows)
-                  console.log("UrlLinkDiv numChd else if -- short link to be deleted-: ct1D1ShrtLnkP"+((iKey -1) - maxRows));//((iKey -1) - maxRows)) 
-                  console.log('UrlLinkDiv numChd '+document.getElementById("ct1D1ShrtLnkP"+((iKey -1) - maxRows)).innerText);//((iKey -1) - maxRows))
-
 
                   prompt(maxnumLnksMsg,"Old link -: https://"+document.getElementById("ct1D1ShrtLnkP"+((iKey -1)  - maxRows)).innerText)
                   
@@ -299,18 +259,14 @@ console.log("UrlLinkDiv -- iKey..:"+iKey)
 
 
 
-//lets update the original ct1D1LnksN1 DIV with new Url before cloning..
-ct1DlongUrlP.textContent=ct1DlongUrl;
-ct1D1ShrtLnkP.textContent=ct1DshortUrl;
 
 
 
 
-if (OnLoadLclStrgFlag){//From OnLoadLclStrg() - Before cloning 'Div\Template' ct1D1LnksN1 below..
+  if (OnLoadLclStrgFlag){//From OnLoadLclStrg() - Before cloning 'Div\Template' ct1D1LnksN1 below..
 
-
-    //When loading from storage we do not have a button id "ct1D1CpyLnkBtn" stored
-    //this will need to be accounted for..
+        //When loading from storage we do not have a button id "ct1D1CpyLnkBtn" stored
+        //this will need to be accounted for..
          if (iKey===0){
               ct1D1CpyLnkBtnId='ct1D1CpyLnkBtn';
               ct1D1ShrtLnkPId='ct1D1ShrtLnkP';
@@ -321,31 +277,32 @@ if (OnLoadLclStrgFlag){//From OnLoadLclStrg() - Before cloning 'Div\Template' ct
 
 
 
- ct1D1CpyLnkBtn.id = ct1D1CpyLnkBtnId;
- ct1D1ShrtLnkP.id = ct1D1ShrtLnkPId;
- // if enabled the last one is sticky, both respond..
- // Note-: sticky buttons can be due to duplicate id`s...
+    ct1D1CpyLnkBtn.id = ct1D1CpyLnkBtnId;
+    ct1D1ShrtLnkP.id = ct1D1ShrtLnkPId;
 
-console.log('UrlLinkDiv OnLoadLclStrgFlag Before Clone iKey-:'+iKey) //btn
-console.log('UrlLinkDiv OnLoadLclStrgFlag Before Clone ct1D1CpyLnkBtn.id-:'+ct1D1CpyLnkBtn.id) //btn
-console.log('UrlLinkDiv OnLoadLclStrgFlag Before Clone ct1D1CpyLnkBtnId-:'+ct1D1CpyLnkBtnId) //btn
+  //  console.log('UrlLinkDiv OnLoadLclStrgFlag Before Clone iKey-:'+iKey) //btn
+  //  console.log('UrlLinkDiv OnLoadLclStrgFlag Before Clone ct1D1CpyLnkBtn.id-:'+ct1D1CpyLnkBtn.id) //btn
+  //  console.log('UrlLinkDiv OnLoadLclStrgFlag Before Clone ct1D1CpyLnkBtnId-:'+ct1D1CpyLnkBtnId) //btn
 
-ct1D1CpyLnkBtnArr.push(ct1D1CpyLnkBtnId,ct1DshortUrl)
-console.log('UrlLinkDiv OnLoadLclStrgFlag ct1D1CpyLnkBtnArr Before Clone -:'+ct1D1CpyLnkBtnArr) //btn
+    ct1D1CpyLnkBtnArr.push(ct1D1CpyLnkBtnId,ct1DshortUrl)
+    console.log('UrlLinkDiv OnLoadLclStrgFlag ct1D1CpyLnkBtnArr Before Clone -:'+ct1D1CpyLnkBtnArr) //btn
 
 
-ct1D1LnksN1.id = 'ct1D1LnksN1'+iKey;//This is for the template..
+  //  ct1D1LnksN1.id = 'ct1D1LnksN1'+iKey;//This is for the template..
 
-}else{//When "Shorten It" button is pushed..
-
-
- //Push the ct1D1CpyLnkBtn.id onto an array to use as a filter for addGlobalEventListener() 
- //that updates/resets buttons textContent and style and copies link to the clipboard when btn is pressed.
-    ct1D1CpyLnkBtnArr.push(ct1D1CpyLnkBtn.id,ct1D1ShrtLnkP.textContent)
-    console.log('UrlLinkDiv ct1D1CpyLnkBtnArr 1 Clone -:'+ct1D1CpyLnkBtnArr)
-}
+  }else{//When "Shorten It" button is pushed..
 
 
+    //Push the ct1D1CpyLnkBtn.id onto an array to use as a filter for addGlobalEventListener() 
+    //that updates/resets buttons textContent and style and copies link to the clipboard when btn is pressed.
+     ct1D1CpyLnkBtnArr.push(ct1D1CpyLnkBtn.id,ct1D1ShrtLnkP.textContent)
+     console.log('UrlLinkDiv ct1D1CpyLnkBtnArr 1 Clone -:'+ct1D1CpyLnkBtnArr)
+  }
+
+
+    //lets update the original ct1D1LnksN1 DIV with new Url before cloning..
+    ct1DlongUrlP.textContent=ct1DlongUrl;
+    ct1D1ShrtLnkP.textContent=ct1DshortUrl;
 
     //Clone div '#ct1D1LnksN1' id (already defined in html & css) to display the links..
     //and display it setting the display param..
@@ -367,8 +324,8 @@ ct1D1LnksN1.id = 'ct1D1LnksN1'+iKey;//This is for the template..
       ct1D1ShrtLnkP.id = 'ct1D1ShrtLnkP'+iKey;
       ct1D1CpyLnkBtn.id = 'ct1D1CpyLnkBtn'+iKey;
       
-      console.log('UrlLinkDiv OnLoadLclStrgFlag After Clone ct1D1CpyLnkBtn.id-:'+ct1D1CpyLnkBtn.id) //btn
-      console.log('UrlLinkDiv OnLoadLclStrgFlag After Clone ct1D1CpyLnkBtnId-:'+ct1D1CpyLnkBtnId) //btn
+//      console.log('UrlLinkDiv OnLoadLclStrgFlag After Clone ct1D1CpyLnkBtn.id-:'+ct1D1CpyLnkBtn.id) //btn
+//      console.log('UrlLinkDiv OnLoadLclStrgFlag After Clone ct1D1CpyLnkBtnId-:'+ct1D1CpyLnkBtnId) //btn
 
      }else{ //update id`s when "Shorten It" button is pushed..
 
@@ -386,11 +343,11 @@ ct1D1LnksN1.id = 'ct1D1LnksN1'+iKey;//This is for the template..
 
   
 
-    console.log('UrlLinkDiv ct1D1LnksN1Clnd.id-:'+ct1D1LnksN1Clnd.id)
-    console.log('UrlLinkDiv ct1DlongUrlP.id-:'+ct1DlongUrlP.id)
-    console.log('UrlLinkDiv ct1D1ShrtLnkP.id-:'+ct1D1ShrtLnkP.id)
-    console.log('UrlLinkDiv ct1D1CpyLnkBtn.id-:'+ct1D1CpyLnkBtn.id)
-    console.log('UrlLinkDiv ct1D1CpyLnkBtnId-:'+ct1D1CpyLnkBtnId)
+//    console.log('UrlLinkDiv ct1D1LnksN1Clnd.id-:'+ct1D1LnksN1Clnd.id)
+//    console.log('UrlLinkDiv ct1DlongUrlP.id-:'+ct1DlongUrlP.id)
+//    console.log('UrlLinkDiv ct1D1ShrtLnkP.id-:'+ct1D1ShrtLnkP.id)
+//    console.log('UrlLinkDiv ct1D1CpyLnkBtn.id-:'+ct1D1CpyLnkBtn.id)
+//    console.log('UrlLinkDiv ct1D1CpyLnkBtnId-:'+ct1D1CpyLnkBtnId)
 
     
     // add it to the DOM after child ct1D1LnksN1
@@ -430,21 +387,21 @@ ct1D1LnksN1.id = 'ct1D1LnksN1'+iKey;//This is for the template..
     //This for loop 1. Checks anything in the array ct1D1CpyLnkBtnArr for just all the Cloned Buttons.
     //2. Copies the short Link. 3. Updates button style. 4. updates textContent.
       for(i=0; i <=ct1D1CpyLnkBtnArr.length; i++){
-          console.log('addGlobalEventListener iKey '+(iKey));
-          console.log('addGlobalEventListener maxRows:'+(maxRows));
-          console.log('addGlobalEventListener i=(iKey - maxRows):'+(iKey - maxRows));
-          console.log('addGlobalEventListener ct1D1CpyLnkBtnArr.length:'+ct1D1CpyLnkBtnArr.length);
-          console.log('addGlobalEventListener');
-          console.log('addGlobalEventListener ct1D1CpyLnkBtnArr[i]:'+ct1D1CpyLnkBtnArr[i]);
-          console.log('addGlobalEventListener ct1D1CpyLnkBtnArr   :'+ct1D1CpyLnkBtnArr);
+  //        console.log('addGlobalEventListener iKey '+(iKey));
+  //        console.log('addGlobalEventListener maxRows:'+(maxRows));
+  //        console.log('addGlobalEventListener i=(iKey - maxRows):'+(iKey - maxRows));
+  //        console.log('addGlobalEventListener ct1D1CpyLnkBtnArr.length:'+ct1D1CpyLnkBtnArr.length);
+  //        console.log('addGlobalEventListener');
+  //        console.log('addGlobalEventListener ct1D1CpyLnkBtnArr[i]:'+ct1D1CpyLnkBtnArr[i]);
+  //        console.log('addGlobalEventListener ct1D1CpyLnkBtnArr   :'+ct1D1CpyLnkBtnArr);
 
           if (ct1D1CpyLnkBtnArr[i]!==undefined&&
               ct1D1CpyLnkBtnArr[i]===e.target.id
               //&&e.target.id==='ct1D1CpyLnkBtn'
               ){
              //It`s always the next elem in the Arr that contains the short Url str..
-             console.log('addGlobalEventListener set btn style -: for button      (ct1D1CpyLnkBtnArr[i]):'+ ct1D1CpyLnkBtnArr[i]);
-             console.log('addGlobalEventListener set btn style -: Str short link  (ct1D1CpyLnkBtnArr[i]+1):'+ ct1D1CpyLnkBtnArr[i+1]);
+  //           console.log('addGlobalEventListener set btn style -: for button      (ct1D1CpyLnkBtnArr[i]):'+ ct1D1CpyLnkBtnArr[i]);
+  //           console.log('addGlobalEventListener set btn style -: Str short link  (ct1D1CpyLnkBtnArr[i]+1):'+ ct1D1CpyLnkBtnArr[i+1]);
              //copy the link to clipboard
               navigator.clipboard.writeText(ct1D1CpyLnkBtnArr[i+1]);
              //set button style
